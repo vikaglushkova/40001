438f51382bfc6695ab8b0ff50373567a574d3450
Vika <vikagluskova2006@gmail.com>
2025-05-14 14:11:02 +0300

    Created new files and wrote main.cpp

--- glushkova.viktoriya/T2/main.cpp
+++ glushkova.viktoriya/T2/main.cpp
@@ -1,2 +1,24 @@
-int main()
-{}
+#include "datastruct.hpp"
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+int main() {
+    std::vector<DataStruct> dataVector;
+
+    std::copy(
+        std::istream_iterator<DataStruct>(std::cin),
+        std::istream_iterator<DataStruct>(),
+        std::back_inserter(dataVector)
+    );
+
+    std::sort(dataVector.begin(), dataVector.end(), compareDataStruct);
+
+    std::copy(
+        dataVector.begin(),
+        dataVector.end(),
+        std::ostream_iterator<DataStruct>(std::cout, "\n")
+    );
+
+    return 0;
+}
