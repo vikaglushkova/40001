1953dc8274063d362c635342b225030dd4e9b6f6
Vika <vikagluskova2006@gmail.com>
2025-06-24 02:44:20 +0300

    Streamline input processing logic

--- glushkova.viktoriya/T2/main.cpp
+++ glushkova.viktoriya/T2/main.cpp
@@ -9,21 +9,24 @@ int main() {
     using custom::DataStruct;
 
     std::vector<DataStruct> data;
+    DataStruct temp;
 
-    while (!std::cin.eof()) {
-        DataStruct temp;
-        if (std::cin >> temp) {
-            data.push_back(temp);
-        } else if (!std::cin.eof()) {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
+    while (std::cin >> temp) {
+        data.push_back(temp);
+    }
+
+    if (std::cin.fail() && !std::cin.eof()) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    if (data.empty()) {
+        std::cerr << "Error: no valid input data\n";
+        return 1;
     }
 
     std::sort(data.begin(), data.end(), custom::compareDataStructs);
-
-    std::copy(data.begin(), data.end(),
-              std::ostream_iterator<DataStruct>(std::cout, "\n"));
+    std::copy(data.begin(), data.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
 
     return 0;
 }
