e61a343fb44e7d5394eb007ea412bcb2bb82828f
Vika <vikagluskova2006@gmail.com>
2025-07-06 11:46:26 +0300

    Fix test validation

--- glushkova.viktoriya/T2/main.cpp
+++ glushkova.viktoriya/T2/main.cpp
@@ -8,41 +8,39 @@
 int main() {
     using custom::DataStruct;
     std::vector<DataStruct> data;
-    bool hasInput = false;
-    bool hasValidRecords = false;
+    bool hasAnyInput = false;
+    bool hasValidData = false;
 
     while (true) {
         DataStruct temp;
         if (std::cin >> temp) {
             data.push_back(temp);
-            hasValidRecords = true;
-            hasInput = true;
+            hasValidData = true;
+            hasAnyInput = true;
         } else if (std::cin.eof()) {
             break;
         } else {
-            hasInput = true;
+            hasAnyInput = true;
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            if (std::cin.eof()) break;
         }
     }
 
-    if (!hasInput) {
+    if (!hasAnyInput) {
         std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
-        return 0;
     }
-    else if (!hasValidRecords) {
+    else if (!hasValidData) {
         std::cout << "Atleast one supported record type\n";
-        return 0;
     }
+    else {
+        std::sort(data.begin(), data.end(), custom::compareDataStructs);
 
-    std::sort(data.begin(), data.end(), custom::compareDataStructs);
-
-    std::copy(
-        data.begin(),
-        data.end(),
-        std::ostream_iterator<DataStruct>(std::cout, "\n")
-    );
+        std::copy(
+            data.begin(),
+            data.end(),
+            std::ostream_iterator<DataStruct>(std::cout, "\n")
+        );
+    }
 
     return 0;
 }
