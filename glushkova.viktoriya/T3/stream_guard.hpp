510943aa8266a56f22eb8b5718ea166974a28d44
Vika <vikagluskova2006@gmail.com>
2025-09-26 20:08:32 +0300

    removed the comments2.0

--- glushkova.viktoriya/T3/stream_guard.hpp
+++ glushkova.viktoriya/T3/stream_guard.hpp
@@ -4,15 +4,15 @@
 
 namespace shapes
 {
-    class StreamGuard   //класс нужен для сохранения потока и автоматически восстанавливает их при выходе из области видимости
+    class StreamGuard
     {
     public:
-        explicit StreamGuard(std::basic_ios< char >& s);   //конструктор, принимает поток по ссылке, сохраняет текущие настройки потокаа в поля  precision_ и flags_   explicit - запрещает неявное преобразование (компилятор автоматически преобразует один тип в другой без явного указания меня)
+        explicit StreamGuard(std::basic_ios< char >& s);
         ~StreamGuard();
     private:
-        std::basic_ios< char >& stream_; //ссылка на поток, для сохранения и восстановления его настроек
-        std::streamsize precision_;  //точность вывода чисел
-        std::basic_ios< char >::fmtflags flags_;   //флаги форматирования
+        std::basic_ios< char >& stream_;
+        std::streamsize precision_;
+        std::basic_ios< char >::fmtflags flags_;
     };
 }
 #endif
