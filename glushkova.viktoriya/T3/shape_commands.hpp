28b8ad0f2d5d1761bfc7987c7498da5b69d191cb
Vika <vikagluskova2006@gmail.com>
2025-09-26 20:05:27 +0300

    removed the comments

--- glushkova.viktoriya/T3/shape_commands.hpp
+++ glushkova.viktoriya/T3/shape_commands.hpp
@@ -5,12 +5,12 @@
 
 namespace shapes
 {
-    void doArea(std::vector< Polygon >& poly, std::istream& in, std::ostream& out);     //& чтобы не копировать весь вектор и изменять исходные данные
-    void doMax(std::vector< Polygon >& poly, std::istream& in, std::ostream& out);
-    void doMin(std::vector< Polygon >& poly, std::istream& in, std::ostream& out);
-    void doCount(std::vector< Polygon >& poly, std::istream& in, std::ostream& out);
-    void doRmecho(std::vector< Polygon >& poly, std::istream& in, std::ostream& out);
-    void doSame(std::vector< Polygon >& poly, std::istream& in, std::ostream& out);
+    void doArea(std::vector<Polygon>& poly, std::istream& in, std::ostream& out);
+    void doMax(std::vector<Polygon>& poly, std::istream& in, std::ostream& out);
+    void doMin(std::vector<Polygon>& poly, std::istream& in, std::ostream& out);
+    void doCount(std::vector<Polygon>& poly, std::istream& in, std::ostream& out);
+    void doRmecho(std::vector<Polygon>& poly, std::istream& in, std::ostream& out);
+    void doSame(std::vector<Polygon>& poly, std::istream& in, std::ostream& out);
 }
 #endif
 
